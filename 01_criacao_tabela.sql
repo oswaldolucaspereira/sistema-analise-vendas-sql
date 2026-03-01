7ad6272e14da6469dd8ad896fc5d027f0e3dcd5a
oswaldolucaspereira <oswaldolucaspereiramonteiro@gmail.com>
2026-03-01 18:03:37 -0300

    Update 01_criacao_tabela.sql
    Adicionando script de criação da tabela sistema_loja.
    DEIXANDO AS COLUNAS TUDO CERTINHO.

--- 01_criacao_tabela.sql
+++ 01_criacao_tabela.sql
@@ -1,1 +1,12 @@
+CREATE TABLE sistema_loja (
+    id_venda INT IDENTITY(1,1) PRIMARY KEY,
+    nome_cliente VARCHAR(100),
+    produto VARCHAR(100),
+    categoria VARCHAR(50),
+    quantidade INT,
+    preco_unitario DECIMAL(10,2),
+    valor_total DECIMAL(10,2),
+    data_venda DATE
+);
+
 
